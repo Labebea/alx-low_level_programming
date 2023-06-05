cf0661b2ac35064bf8aad8a55d27af8cdd464958
Labebea <larylabebeatrice@gmail.com>
2023-06-05 02:16:53 -0700

    printing times table and the sum of some numbers

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table 
+ * @n: integer
+ * Return: no value
+ */
+void print_times_table(int n)
+{
+	if (n > 0 && n < 15)
+	{
+		int i, j, k;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (k < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + k);
+				}
+				else if (k > 9 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + k / 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + k / 100);
+					_putchar('0' + (k % 100) / 10);
+					_putchar('0' + (k % 100) % 10);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else if (n < 0 || n > 14)
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the multipkes of 3 or 5 below 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int sum;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum = sum + n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
